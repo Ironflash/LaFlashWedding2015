d481774028b9fae4f77b4f35dbc0aab117038976
Gregory <gregorylaflash@gmail.com>
2013-11-27 21:14:51 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaFlashWedding2015::Application.config.secret_key_base = '936fffc8b78d9423bb24bb5f274d36120fc233245edd10795cbdd9b6a9ca5f105e7f78c16db7e2ad018cfbd713879b2aa6a027d64cad144a2c5782f7e75036b8'
